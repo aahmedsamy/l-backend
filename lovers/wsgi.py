97abe59122a55ae30ef6bd304bc51fc6d612841f
Ahmed Samy <aahmedsamy.as@gmail.com>
2019-10-07 13:15:10 +0200

    rename the project

--- lovers/wsgi.py
+++ lovers/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for lovers_connections project.
+WSGI config for lovers project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lovers_connections.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lovers.settings')
 
 application = get_wsgi_application()
