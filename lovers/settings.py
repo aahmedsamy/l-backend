6645392146a532c3d903138a963548e7d769ca00
Ahmed Samy <aahmedsamy.as@gmail.com>
2019-10-08 12:20:50 +0200

    add cors, fix bug in LoginSerializer, login views

--- lovers/settings.py
+++ lovers/settings.py
@@ -45,9 +45,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -165,11 +165,6 @@ JWT_AUTH = {
 ################################
 
 # CORS HEADERS #################
-CORS_ORIGIN_ALLOW_ALL = False
-
-CORS_ORIGIN_WHITELIST = [
-    "http://127.0.0.1:3000",
-    "https://lovers.ahmedsamy.me",
-]
-
+ALLOWED_HOSTS = ["*"]
+CORS_ORIGIN_ALLOW_ALL = True
 ################################
